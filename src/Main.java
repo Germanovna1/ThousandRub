9a18753873f907efb82a5b2108c7cab9e7187163
germanovna1 <vatakivi@yandex.ru>
2022-11-26 16:28:32 +0300

    Formated code

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
         int amount = 1100;
 
         int bonus;
-            if (amount > 1000) {
-                bonus = 1;
-        } else{
+        if (amount > 1000) {
+            bonus = 1;
+        } else
             bonus = 0;
-        }
+
         int total = amount / 100 * bonus;
         System.out.println("Итого бонусов:" + total);
 
         int total2 = wallet + amount;
 
         System.out.println("Итого сумма в руб.:" + total2);
-        }
     }
+}
